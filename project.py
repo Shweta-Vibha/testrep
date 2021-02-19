27db0ce44765707e1666198fe5c11d743d5047b9
Shweta-Vibha <shwetavibhaktcs@gmail.com>
2021-02-19 09:12:12 +0530

    commiting merged file

--- project.py
+++ project.py
@@ -9,13 +9,6 @@ def fun2():
     n2= 6
     sum = n1+n2
     print('The sum of {0} and {1} is {2}'.format(n1, n2, sum))
-    
-
-     
-
-
-def fun1():
-	print("Hello!")
 
 //Shweta. Branch_image
 def fun3():

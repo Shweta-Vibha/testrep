382a78307c6f31b94fa3ebbb7a5e17c90712e436
Shweta-Vibha <shwetavibhatkcs@gmail.com>
2021-02-19 08:55:50 +0530

    Second function

--- project.py
+++ project.py
@@ -1,2 +1,18 @@
-def fun1():
+//Vibha
+def fun1():        
 	print("Hello!")
+
+//Shweta
+def fun2():
+    n1= 4
+    n2= 6
+    sum = n1+n2
+    print('The sum of {0} and {1} is {2}'.format(n1, n2, sum))
+    
+//Shweta
+def fun3():
+     n = 8
+     n_sqrt = n ** 0.5
+     print('The sq rt of %0.3f is %0.3f'.%(n, n_sqrt))
+     
+
